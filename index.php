61a97d359c5bdd918058b69b103dbae4dc079aac
David Velasquez <40569893+davidve0206@users.noreply.github.com>
2024-10-15 11:11:10 +0100

    feat: drop existing database and create new tables on start (#2)

--- index.php
+++ index.php
@@ -1,6 +1,9 @@
 <?php
+  ini_set('display_errors', 1);
+  ini_set('display_startup_errors', 1);
+  error_reporting(E_ALL);
   // For now, index.php just redirects to browse.php, but you can change this
   // if you like.
-  
+  require "database/setup.php";
   header("Location: views/browse.php");
 ?>
