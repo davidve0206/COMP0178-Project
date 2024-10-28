9336af9af367c8eadde3acb8c9b5cb95a7614e99
CharlesRA752 <rassmusscharles@yahoo.com>
2024-10-28 10:42:30 +0000

    Changed a typo and created provisional user table (#6)
    * Changed a typo and created provisional user table
    
    * Changed to camel case, and changed the id
    
    * Found an instance where the migrate function was called

--- database/migrations.php
+++ database/migrations.php
@@ -5,7 +5,7 @@
  * @param mysqli $db
  * @return void
  */
-function migate(mysqli $db)
+function migrate(mysqli $db)
 {
     $db_name = "auction_site";
 
@@ -25,9 +25,16 @@ function migate(mysqli $db)
     // Create the tables we need
     $create_users_statement = "
     CREATE TABLE Users
-    (
-    id INTEGER AUTO_INCREMENT PRIMARY KEY,
-    username VARCHAR(40) NOT NULL
+    (id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(30) NOT NULL,
+    password VARCHAR(30) NOT NULL,
+    email VARCHAR(30) NOT NULL,
+    firstName VARCHAR(20) NOT NULL,
+    lastName VARCHAR(30) NOT NULL,
+    address VARCHAR(100) NOT NULL,
+    userJoinTime TIMESTAMP(0) NOT NULL DEFAULT CURRENT_TIMESTAMP(0),
+    isBuyer BOOLEAN NOT NULL DEFAULT True, 
+    isSeller BOOLEAN NOT NULL DEFAULT False 
     )";
     $db->query($create_users_statement);
 
