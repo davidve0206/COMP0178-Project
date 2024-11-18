d753682389ccdfef76d9c84c48b2375f1d780c3d
CharlesRA752 <rassmusscharles@yahoo.com>
2024-11-18 15:38:22 +0000

    Cleanup - thought I'd have to do more here, and I had some comments to reflect that

--- views/watchlist_funcs.php
+++ views/watchlist_funcs.php
@@ -1,7 +1,4 @@
  <?php
-  require_once "mailer.php";
-  // Just a general note: I think I might have to move the second half of this code. This page gets called in listings.php, 
-  // which I think means that it all runs there, which isn't consistent with me defining events etc. 
 
   // Follow or unfollow an item ()
   if (!isset($_POST['functionname']) || !isset($_POST['arguments'])) {
@@ -40,34 +37,5 @@
   // so be careful. You can also return JSON objects (in string form) using
   // echo json_encode($res).
   echo $res;
-
-  // Events 
-
-  // Event: Auction closing 
-  // TODO: define the condition that has to be met here
-
-
-  // Event: 
-  //TODO: I need to check that what I have in mind here is consistent with what David has already done
-  // Set out the body and heading (strings) for each of the events that we've defined: Closing an auction (Winner and not-winner) and Being outbid 
-
-  // Define the functions that we'll use to send notificaitons 
-
-  //TODO: Define a send email protocol (to be called later)
-  //TODO: Define a protocol for storing a notification in the notificaiotns table
-
-  // Sending notificaitons
-  // Create a function (triggered elsewhere) which sends both an email and notification in the event of an auction closing 
-
-  // Event: Auction closing
-  // SQL query that checks whether the userId is not on the winning bid, and then whether it is on the winning bid
-  // Sends inserts the relevant variable into the email    
-
-  // Event: Outbid by someone else 
-  // Triggers elsewhere, but if this function is triggered: inserts the relevant text into a notification, and sends them.
-
-
-  // N.B. I'm not sure of the ordering between Notification functions and sending notifications. 
-  // It might be better to use sending notifications to outline a set of variables, which then get inserted into the function... Eh, not a big issue rn. 
 
   ?>
