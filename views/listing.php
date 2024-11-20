f5c12286028721e9841650e2022b358724657ffb
davidve0206 <davidve0206@gmail.com>
2024-11-20 16:40:10 +0000

    implement difference in listing view for buyers and sellers

--- views/listing.php
+++ views/listing.php
@@ -22,7 +22,9 @@
   $auction = $result->fetch_assoc();
 
   // Fetch current bid
-  $query_bids = "SELECT bidPrice, bidderId, isHighest, isWinner AS current_bid FROM Bids WHERE itemId = ? ORDER BY bidPrice DESC";
+  $query_bids = "SELECT bidPrice, bidderId, isHighest, isWinner, Users.username
+                  FROM Bids JOIN Users ON Bids.bidderId = Users.id
+                  WHERE itemId = ? ORDER BY bidPrice DESC";
   $stmt_bids = $db->prepare($query_bids);
   $stmt_bids->bind_param("i", $item_id);
   $stmt_bids->execute();
@@ -32,6 +34,7 @@
   $current_price = $highest_bid['bidPrice'] ?? $auction['startPrice'];
   $title = $auction['itemName'];
   $description = $auction['description'];
+  console_log($highest_bid);
 
   // TODO: Note: Auctions that have ended may pull a different set of data,
   //       like whether the auction ended in a sale or was cancelled due
@@ -68,8 +71,8 @@
           <p class="text-muted">Seller: <?php echo htmlspecialchars($auction['username']); ?></p>
           <p class="text-muted">Starting bid: £<?php echo number_format($auction['startPrice'], 2); ?></p>
           <p class="text-muted">Number of bids: <?php echo number_format($total_bids); ?></p>
-
-          <?php if ($now < $end_time): ?>
+          
+          <?php if (isset($_SESSION['userId']) && $_SESSION['isBuyer'] && $now < $end_time): ?>
             <div id="watch_nowatch" <?php if ($has_session && $watching) echo('style="display: none"');?> >
               <button type="button" class="btn btn-outline-secondary btn-sm" onclick="addToWatchlist()">+ Add to watchlist</button>
             </div>
@@ -84,6 +87,14 @@
         <div class="col-md-4">
           <?php if ($now > $end_time): ?>
             <p>This auction ended <?php echo(date_format($end_time, 'j M H:i')); ?></p>
+            <?php if (isset($_SESSION['userId']) && intval($_SESSION['userId']) == $auction['sellerId']): ?>
+              <p class="text-muted"><?php echo(
+                isset($highest_bid)
+                ? ($highest_bid['isWinner'] ? 'Winning bid was ' : 'Highest bid was ')
+                  . number_format($current_price, 2) . ' by ' . $highest_bid['username']
+                : 'No bids were placed.'
+                ) ?></p>
+            <?php endif; ?>
           <?php else: ?>
             <?php if ($total_bids > 0): ?>
               <p class="text-muted mb-0">Recent bids:</p>
@@ -100,16 +111,22 @@
               </ul>
             <?php endif; ?>
             <p class="lead">Current bid: £<?php echo(number_format($current_price, 2)); ?></p>
-            <form method="POST" action="place_bid.php">
-              <div class="input-group mb-3">
-                <div class="input-group-prepend">
-                  <span class="input-group-text">£</span>
-                </div>
-                <!-- Check to make sure the bid is higher than the current price -->
-                <input type="number" class="form-control" id="bid" name="bid" min="<?php echo $current_price + 1; ?>" required>
-              </div>
-              <button type="submit" class="btn btn-primary form-control">Place bid</button>
-            </form>
+            <?php if (isset($_SESSION['userId']) && $_SESSION['isBuyer']): ?>
+              <?php if (!isset($highest_bid) || intval($_SESSION['userId']) != $highest_bid['bidderId']): ?>
+                <form method="POST" action="place_bid.php">
+                  <div class="input-group mb-3">
+                    <div class="input-group-prepend">
+                      <span class="input-group-text">£</span>
+                    </div>
+                    <!-- Check to make sure the bid is higher than the current price -->
+                    <input type="number" class="form-control" id="bid" name="bid" min="<?php echo $current_price + 1; ?>" required>
+                  </div>
+                  <button type="submit" class="btn btn-primary form-control">Place bid</button>
+                </form>
+              <?php else: ?>
+                <p class="font-weight-bold text-primary">You are the highest bidder.</p>
+              <?php endif; ?> 
+            <?php endif; ?>
           <?php endif; ?>
         </div>
       </div> <!-- End of row -->
