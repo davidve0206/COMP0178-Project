442bc413d726feff20eafb8cfe8e70c053d32531
Doğukan Türköz <dogukanturkoz14@gmail.com>
2024-11-24 12:27:45 +0000

    registration and images for default auctions (#26)
    * images added for all default auctions
    
    * don't try to display image if auction has no image
    
    * added neccessary fields to the form to match with the database
    
    * registration now working

--- views/register.php
+++ views/register.php
@@ -16,27 +16,59 @@
           <input class="form-check-input" type="radio" name="accountType" id="accountSeller" value="seller" onclick="toggleRadioRequired()">
           <label class="form-check-label" for="accountSeller">Seller</label>
         </div>
+        <div class="form-check form-check-inline">
+          <input class="form-check-input" type="radio" name="accountType" id="accountMixed" value="mixed" onclick="toggleRadioRequired()">
+          <label class="form-check-label" for="accountMixed">Mixed</label>
+        </div>
         <small id="accountTypeHelp" class="form-text-inline text-muted"><span class="text-danger">* Required.</span></small>
+      </div>
+    </div>
+    <div class="form-group row">
+      <label for="firstName" class="col-sm-2 col-form-label text-right">First Name</label>
+      <div class="col-sm-10">
+        <input type="text" class="form-control" id="firstName" name="firstName" placeholder="First Name" oninput="toggleRequired('fnHelp', this)">
+        <small id="fnHelp" class="form-text text-muted"><span class="text-danger">* Required.</span></small>
+      </div>
+    </div>
+    <div class="form-group row">
+      <label for="lastName" class="col-sm-2 col-form-label text-right">Last Name</label>
+      <div class="col-sm-10">
+        <input type="text" class="form-control" id="lastName" name="lastName" placeholder="Last Name" oninput="toggleRequired('lnHelp', this)">
+        <small id="lnHelp" class="form-text text-muted"><span class="text-danger">* Required.</span></small>
+      </div>
+    </div>
+    <div class="form-group row">
+      <label for="username" class="col-sm-2 col-form-label text-right">Username</label>
+      <div class="col-sm-10">
+        <input type="text" class="form-control" id="username" name="username" placeholder="Username" oninput="toggleRequired('usernameHelp', this)">
+        <small id="usernameHelp" class="form-text text-muted"><span class="text-danger">* Required.</span></small>
       </div>
     </div>
     <div class="form-group row">
       <label for="userEmail" class="col-sm-2 col-form-label text-right">Email</label>
       <div class="col-sm-10">
-        <input type="text" class="form-control" id="userEmail" placeholder="Email" oninput="toggleRequired('emailHelp', this)">
+        <input type="text" class="form-control" id="userEmail" name="userEmail" placeholder="Email" oninput="toggleRequired('emailHelp', this)">
         <small id="emailHelp" class="form-text text-muted"><span class="text-danger">* Required.</span></small>
+      </div>
+    </div>
+    <div class="form-group row">
+      <label for="address" class="col-sm-2 col-form-label text-right">Address</label>
+      <div class="col-sm-10">
+        <input type="text" class="form-control" id="address" name="address" placeholder="Address" oninput="toggleRequired('addressHelp', this)">
+        <small id="addressHelp" class="form-text text-muted"><span class="text-danger">* Required.</span></small>
       </div>
     </div>
     <div class="form-group row">
       <label for="initialPassword" class="col-sm-2 col-form-label text-right">Password</label>
       <div class="col-sm-10">
-        <input type="password" class="form-control" id="initialPassword" placeholder="Password" oninput="toggleRequired('passwordHelp', this)">
+        <input type="password" class="form-control" id="initialPassword" name="initialPassword" placeholder="Password" oninput="toggleRequired('passwordHelp', this)">
         <small id="passwordHelp" class="form-text text-muted"><span class="text-danger">* Required.</span></small>
       </div>
     </div>
     <div class="form-group row">
       <label for="repeatPassword" class="col-sm-2 col-form-label text-right">Repeat password</label>
       <div class="col-sm-10">
-        <input type="password" class="form-control" id="repeatPassword" placeholder="Enter password again" oninput="toggleRequired('repeatPasswordHelp', this)">
+        <input type="password" class="form-control" id="repeatPassword" name="repeatPassword" placeholder="Enter password again" oninput="toggleRequired('repeatPasswordHelp', this)">
         <small id="repeatPasswordHelp" class="form-text text-muted"><span class="text-danger">* Required.</span></small>
       </div>
     </div>
@@ -60,9 +92,10 @@
       const repeatPassword = document.getElementById("repeatPassword").value;
       const accountBuyer = document.getElementById("accountBuyer").checked;
       const accountSeller = document.getElementById("accountSeller").checked;
+      const accountMixed = document.getElementById("accountMixed").checked;
 
       // Account type check
-      if (!accountBuyer && !accountSeller) {
+      if (!accountBuyer && !accountSeller && !accountMixed) {
         alert("Please select an account type.");
         return false;
       }
