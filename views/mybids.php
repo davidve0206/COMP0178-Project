0dd6620e58f6ca527d215b12d7e852c7003bbaaa
Ben Thompson <80930461+bthompson-dev@users.noreply.github.com>
2024-11-24 12:30:52 +0000

    Recommendations + Pagination (#25)
    * added pagination to mybids and mylistings, split it into utils file
    
    * removed uploaded image
    
    * working recommendations

--- views/mybids.php
+++ views/mybids.php
@@ -7,34 +7,67 @@
   <h2 class="my-3">My bids</h2>
 
   <div class="container mt-5">
-  <?php
-  // This page is for showing a user the auctions they've bid on.
-  // It will be pretty similar to browse.php, except there is no search bar.
-  // This can be started after browse.php is working with a database.
-  // Feel free to extract out useful functions from browse.php and put them in
-  // the shared "utilities.php" where they can be shared by multiple files.
+    <?php
+    // This page is for showing a user the auctions they've bid on.
+    // It will be pretty similar to browse.php, except there is no search bar.
+    // This can be started after browse.php is working with a database.
+    // Feel free to extract out useful functions from browse.php and put them in
+    // the shared "utilities.php" where they can be shared by multiple files.
 
 
-  // Check user's credentials (cookie/session).
-  if (!isset($_SESSION['userId'])) {
-    echo 'You must be logged in to view bids.';
-  } else {
-    $user_id = intval($_SESSION['userId']);
+    // Check user's credentials (cookie/session).
+    if (!isset($_SESSION['userId'])) {
+      echo 'You must be logged in to view bids.';
+    } else {
+      $user_id = intval($_SESSION['userId']);
 
-    if ($user_id == 0) {
-      echo 'Invalid user ID.';
+      if ($user_id == 0) {
+        echo 'Invalid user ID.';
+      }
     }
-  }
 
-  // Perform a query to pull up the auctions they've bidded on.
-  $db->query("USE auction_site");
+    if (!isset($_GET['page'])) {
+      $curr_page = 1;
+    } else {
+      $curr_page = $_GET['page'];
+    }
 
-  $query = construct_listings_query(null, null, "endDate", $user_id, null);
-  $result = mysqli_query($db, $query);
 
-  // Loop through results and print them out as list items.
-  listings_loop($db, $result);
-  ?>
+    // Perform a query to pull up the auctions they've bidded on.
+    $db->query("USE auction_site");
+
+    $query = construct_listings_query(null, null, "endDate", $user_id, null);
+    $result = mysqli_query($db, $query);
+
+    /* Working out total number of results that satisfy the above query so that pages can be displayed correctly */
+    $num_results = $result->num_rows;
+    $results_per_page = 10;
+    $max_page = ceil($num_results / $results_per_page);
+    $offset = 10 * ($curr_page - 1);
+
+    // Limit results by page
+    $limit_query = $query . " LIMIT 10 OFFSET $offset;";
+
+    $limit_result = mysqli_query($db, $limit_query);
+    ?>
+
+    <div class="container mt-5">
+
+      <!-- If result set is empty, print an informative message -->
+
+      <?php
+      if ($num_results == 0) {
+
+        echo '<p> No results found. </p>';
+      } else {
+        listings_loop($db, $limit_result);
+      }
+      ?>
+
+      <?php include_once('../utils/pagination.php') ?>
+
+
+    </div>
 
   </div>
 
--- views/recommendations.php
+++ views/recommendations.php
@@ -1,22 +1,82 @@
-<?php include_once("header.php")?>
-<?php require("../utils/utilities.php")?>
+<?php include_once("header.php") ?>
+<?php require("../utils/utilities.php") ?>
+<?php require_once("../database/setup.php") ?>
 
 <div class="container">
 
-<h2 class="my-3">Recommendations for you</h2>
+  <h2 class="my-3">Recommendations for you</h2>
 
-<?php
+  <?php
   // This page is for showing a buyer recommended items based on their bid 
   // history. It will be pretty similar to browse.php, except there is no 
   // search bar. This can be started after browse.php is working with a database.
   // Feel free to extract out useful functions from browse.php and put them in
   // the shared "utilities.php" where they can be shared by multiple files.
-  
-  
-  // TODO: Check user's credentials (cookie/session).
-  
-  // TODO: Perform a query to pull up auctions they might be interested in.
-  
-  // TODO: Loop through results and print them out as list items.
-  
-?>
+
+
+  // Check user's credentials (cookie/session).
+  if (!isset($_SESSION['userId'])) {
+    echo 'You must be logged in to get recommendations.';
+  } else {
+    $user_id = intval($_SESSION['userId']);
+
+    if ($user_id == 0) {
+      echo 'Invalid user ID.';
+    }
+  }
+
+  if (!isset($_GET['page'])) {
+    $curr_page = 1;
+  } else {
+    $curr_page = $_GET['page'];
+  }
+
+  // Perform a query to pull up auctions they might be interested in.
+  $db->query("USE auction_site");
+
+  $query = "SELECT id, itemName, description, endDate, GREATEST(startPrice, IFNULL(MAX(bidPrice), startPrice)) AS currentPrice FROM Items, Bids
+            WHERE Items.id = Bids.itemId
+            AND Bids.bidderId IN (
+                SELECT bidderId FROM Bids
+                WHERE itemId IN (
+                  SELECT Items.id FROM Items, Bids
+                  WHERE Items.id = Bids.itemId
+                  AND Bids.bidderId = $user_id)
+                AND bidderId <> $user_id )
+            AND Items.id NOT IN (
+                SELECT Items.id FROM Items, Bids
+                WHERE Items.id = Bids.itemId
+                AND Bids.bidderId = $user_id)
+            GROUP BY Items.id";
+
+  $result = mysqli_query($db, $query);
+
+  $num_results = $result->num_rows;
+  $results_per_page = 10;
+  $max_page = ceil($num_results / $results_per_page);
+  $offset = 10 * ($curr_page - 1);
+
+  // Limit results by page
+  $limit_query = $query . " LIMIT 10 OFFSET $offset;";
+
+  $limit_result = mysqli_query($db, $limit_query);
+  ?>
+
+  <div class="container mt-5">
+
+    <!-- If result set is empty, print an informative message -->
+
+    <?php
+    if ($num_results == 0) {
+
+      echo '<p> No recommendations found! You must bid on an item before we can give you recommendations. </p>';
+    } else {
+      listings_loop($db, $limit_result);
+    }
+    ?>
+
+    <?php include_once('../utils/pagination.php') ?>
+
+
+  </div>
+</div>
